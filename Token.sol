066512e658b4f60abb996c988e2c31e362c18f3b
Nari <nuyhank@gmail.com>
2025-08-01 16:49:38 +0900

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -1,10 +1,18 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Token is ERC20 {
-    constructor(uint256 initialSupply) ERC20("WonRepo", "WRP") {
-        _mint(msg.sender, initialSupply);
+contract WRPToken is ERC20 {
+    address public admin;
+
+    constructor(uint256 initialSupply) ERC20("Won Repo", "WRP") {
+        admin = msg.sender;
+        _mint(admin, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) external {
+        require(msg.sender == admin, "Only admin can mint");
+        _mint(to, amount);
     }
 }
